08444e9034e129713f2a0ea7937825c90610aa6e
cleitson <cleitson.ftw@gmail.com>
2024-06-13 18:34:56 -0300

    feat: nova pagina de contato e resposta

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
+import Thanks from './pages/Thanks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path='/about' element={<About />} />
       <Route path='/projects' element={<Projects />} />
       <Route path='/contact' element={<Contact />} />
+      <Route path='/thanks' element={<Thanks />} />
     </Routes>
     </div>
   )
