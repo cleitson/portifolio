359f27c47ef9a6bf006ad76db82b8a66bc0e66e9
cleitson <cleitson.ftw@gmail.com>
2024-06-25 20:55:26 -0300

    refactor: transformando todas as paginas em uma single-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import Thanks from './pages/Thanks'
-import Header from './components/Header/Header'
 
 function App() {
-
   return (
-    <div className='bg-[#070D0D] text-white'>
-    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/projects' element={<Projects />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/thanks' element={<Thanks />} />
     </Routes>
-    </div>
   )
 }
 
