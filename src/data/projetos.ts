51892315e23b944e5822694be1010cef34d3e1d6
Cleitson Lima <cleitson.ftw@gmail.com>
2025-02-11 10:55:45 -0300

    Update projetos.ts

--- src/data/projetos.ts
+++ src/data/projetos.ts
@@ -43,7 +43,7 @@ export const Voluntario: ProjetoType[] = [
   {
     id: 1,
     title: 'Associação Beneficente A Força do Amor',
-    description: 'Voluntariado remoto dedicado ao desenvolvimento de um website institucional responsivo e funcional para a FAM. Empregando ferramentas como HTML, CSS, JavaScript. Com este projeto, espera-se aumentar a visibilidade da ONG, facilitar o acesso a informações e serviços, e promover uma maior participação da comunidade. (Em desenvolvimento)',
+    description: 'Voluntariado remoto dedicado ao desenvolvimento de um website institucional responsivo e funcional para a FAM. Empregando ferramentas como HTML, CSS, JavaScript. Com este projeto, espera-se aumentar a visibilidade da ONG, facilitar o acesso a informações e serviços, e promover uma maior participação da comunidade.',
     image: img,
     stack: ['Html','Css','Javascript', 'NodeJs'],
     link: 'https://www.aforcadoamor.org.br/',
